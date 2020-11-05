9ecda9776e73d1f577a9cc3c16946913864fc6a6
Yoba91 <49066344+Yoba91@users.noreply.github.com>
2020-11-05 17:23:21 +0300

    Добавьте файлы проекта.

---
+++ TestProjectASP.NET_MVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TestProjectASP.NET_MVC.Models;
+
+namespace TestProjectASP.NET_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Greeting = DateTime.Now.Hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("Index");
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ TestProjectASP.NET_MVC/Models/Repository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestProjectASP.NET_MVC.Models
+{
+    public class Repository
+    {
+        private static IList<GuestResponse> responses = new List<GuestResponse>();
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get { return responses; }
+        }
+        public static void AddResponse (GuestResponse guestResponse)
+        {
+            responses.Add(guestResponse);
+        }
+    }
+}
